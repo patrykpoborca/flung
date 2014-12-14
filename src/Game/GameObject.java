0a8c9c8a9b9ffbdd14c97ea3c9279160a8a6136a
Patryk Jacek Poborca <poborcapatryk@gmail.com>
2014-12-14 15:16:44 -0600

    Initial Commit

---
+++ src/Game/GameObject.java
@@ -0,0 +1,10 @@
+package Game;
+
+import java.util.Vector;
+
+public class GameObject {
+
+	private	Vector<int> position;
+	
+	
+}
---
+++ src/utility/GameConstants.java
@@ -0,0 +1,8 @@
+package utility;
+
+import java.util.ArrayList;
+
+final public class GameConstants {
+	
+	public ArrayList gameObjects = new ArrayList();
+}
